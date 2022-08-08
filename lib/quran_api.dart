010bb99fdf2eb7007d3a8c3a76ba043fc4bebd61
AbdullahHalari <droidabdullah@gmail.com>
2022-08-09 03:37:48 +0500

    Surah...

--- lib/quran_api.dart
+++ lib/quran_api.dart
@@ -9,6 +9,9 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+// import 'package:quranapp/ayat.dart';
+import 'package:quranapp/reset.dart';
+// import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 
 class Quran_api extends StatefulWidget {
   const Quran_api({Key? key}) : super(key: key);
@@ -19,18 +22,29 @@ class Quran_api extends StatefulWidget {
 
 class _Quran_apiState extends State<Quran_api> {
   // getuser() async {
-  final topUrl = "https://raw.githubusercontent.com/AbdullahHalari/Quran_api/master/surah_api.json";
+  final topUrl =
+      "https://raw.githubusercontent.com/AbdullahHalari/Quran_api/master/quran2.json";
+  // final nexturl =
+  //     "https://quranenc.com/api/v1/translation/sura/urdu_junagarhi/2";
+  // late PDFDocument document;
 
   Future<List<usermodel>> getArticle() async {
     Response res = await get(Uri.parse(topUrl));
+    // Response res1 = await get(Uri.parse(nexturl));
 
     if (res.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(res.body);
+      // Map<String, dynamic> json1 = jsonDecode(res1.body);
 
-      List<dynamic> body = json['data'];
+      List<dynamic>body = json['data'];
+      // List<dynamic> body1 = json1['results'];
 
       List<usermodel> articles =
           body.map((dynamic item) => usermodel.fromJson(item)).toList();
+      // List<Result> articles1 =
+      //     body1.map((dynamic item) => Result.fromJson(item)).toList();
+
+      // print(articles1);
       print(articles);
       return articles;
     } else {
@@ -55,60 +69,69 @@ class _Quran_apiState extends State<Quran_api> {
                 return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (context, i) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        // radius: 20,
-                        backgroundColor: Color.fromARGB(255, 4, 56, 100),
-                        // backgroundImage: AssetImage('images/quran.png'),
-                        child: ClipOval(
-                            child: Text(
-                          snapshot.data[i].number.toString() != null
-                              ? snapshot.data[i].number.toString()
-                              : '00',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 15.0,
-                          ),
-                        )
-                            //  Image.asset(
-                            //   'images/quran.png',
-                            //   fit: BoxFit.fill,
-                            // ),
-                            ),
-                      ),
-                      title: Text(
-                        snapshot.data[i].name != null
-                            ? snapshot.data[i].name
-                            : 'سُوْرَۃُ...',
+                    return InkWell(
+                        // onTap: () {
+                        //   document = PDFDocument.fromURL(snapshot.data[i].pdf != null
+                        //           ? snapshot.data[i].pdf
+                        //           : 'wait..',) as PDFDocument;
+                        // },
+                        child: ListTile(
+                            leading: CircleAvatar(
+                              // radius: 20,
+                              // backgroundColor: Color.fromARGB(255, 4, 56, 100),
+                              backgroundImage: AssetImage('images/1.png'),
+                              child: ClipOval(
+                                  child: Text(
+                                snapshot.data[i].number.toString() != null
+                                    ? snapshot.data[i].number.toString()
+                                    : '00',
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 20.0,
-                                ),
-                      ),
-                      subtitle: Text(
-                        
-                                    snapshot.data[i].englishNameTranslation !=
-                                null
-                            ? snapshot.data[i].englishNameTranslation
-                            : 'Not Found',
-                                style: TextStyle(
+                                  color: Color.fromARGB(255, 255, 255, 255),
                                   fontSize: 15.0,
                                 ),
-                      ),
-                      // trailing:
-                      trailing:  Text(
-                          "verses: ${snapshot.data[i].numberOfAyahs.toString() != null
-                              ? snapshot.data[i].numberOfAyahs.toString()
-                              : '00'}",
-                          style: TextStyle(
-                            // fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 15.0,
-                          ),
-                        
-                      ),
-                    );
+                              )
+                                  //  Image.asset(
+                                  //   'images/quran.png',
+                                  //   fit: BoxFit.fill,
+                                  // ),
+                                  ),
+                            ),
+                            title: Text(
+                              snapshot.data[i].englishName != null
+                                  ? snapshot.data[i].englishName
+                                  : 'wait..',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            subtitle: Text(
+                              snapshot.data[i].englishNameTranslation != null
+                                  ? snapshot.data[i].englishNameTranslation
+                                  : 'Not Found',
+                              style: TextStyle(
+                                fontSize: 15.0,
+                              ),
+                            ),
+                            trailing:
+                                // Padding(
+                                //   padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                                // child:
+                                Container(
+                              width: 90,
+                              child: Text(
+                                snapshot.data[i].name != null
+                                    ? snapshot.data[i].name
+                                    : 'سُوْرَۃُ...',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 15.0,
+                                  // ),
+                                ),
+                              ),
+                            )));
                   },
                 );
             }));
@@ -116,23 +139,59 @@ class _Quran_apiState extends State<Quran_api> {
 }
 
 class usermodel {
+  // Ayahs ayahs;
   String name;
   String englishNameTranslation;
+  String englishName;
   int number;
-  int numberOfAyahs;
+  String pdf;
+  // int numberOfAyahs;
 
   usermodel({
+    // required this.ayahs,
     required this.name,
     required this.englishNameTranslation,
     required this.number,
-    required this.numberOfAyahs,
+    required this.englishName,
+    required this.pdf,
+    // required this.numberOfAyahs,
   });
   factory usermodel.fromJson(Map<String, dynamic> json) {
     return usermodel(
+      // ayahs: Ayahs.fromJson(json['ayahs']),
       name: json['name'] as String,
       englishNameTranslation: json['englishNameTranslation'] as String,
+      englishName: json['englishName'] as String,
       number: json['number'],
-      numberOfAyahs: json['numberOfAyahs'],
+      pdf: json['pdf']
+    );
+  }
+}
+
+class Result {
+  String id;
+  String sura;
+  String aya;
+  String arabicText;
+  String translation;
+  String footnotes;
+
+  Result(
+      {required this.id,
+      required this.sura,
+      required this.aya,
+      required this.arabicText,
+      required this.translation,
+      required this.footnotes});
+
+  factory Result.fromJson(Map<String, dynamic> json) {
+    return Result(
+      id: json['id'],
+      sura: json['sura'],
+      aya: json['aya'],
+      arabicText: json['arabic_text'],
+      translation: json['translation'],
+      footnotes: json['footnotes'],
     );
   }
 }
